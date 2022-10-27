9674a680e0a258e27a91b9ae3455e519f4527592
Joey Racz <jjracz0621@eagle.fgcu.edu>
2022-10-27 19:37:54 -0400

    Added bot commands for getting calendar credentials and talking to the chatbot

--- main.py
+++ main.py
@@ -3,17 +3,16 @@ import json
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+import calendar_work
+import discord_bot
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
-bot = commands.Bot(command_prefix='!', intents=discord.Intents.default())
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='!', intents=intents)
+
 
 @bot.event
 async def on_ready():
@@ -21,55 +20,16 @@ async def on_ready():
 
 
 # Command for getting user's credentials for their Google calendar:
-"""
-    # TODO: Assign username of person calling command to name (change next line)
-    name = "John"
-    creds = None
+@bot.command(name='calendar')
+async def calendar_creds(ctx):
+    author = str(ctx.message.author)
+    calendar_work.get_credentials(author)
 
-    if os.path.exists('tokens.json'):
-        # Search through file to find if the user already has a registered key.
-        with open('tokens.json', 'r') as tokens:
-            data = json.load(tokens)
-            for key in data:
-                if key == name:
-                    # Write the user's credentials to a json file to be copied to creds.
-                    with open('token.json', 'w') as token:
-                        json.dump(data[key], token)
-                    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-                    break
 
-    # TODO: Ensure requests are visible/appear to the user instead of the bot.
-    if not creds or not creds.valid:
-        # Send a request to refresh credentials if they are expired.
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        # Send a request to ask for the user's credentials if the user does not have any registered.
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+# Command for starting a conversation with the bot
+@bot.command(name='chatbot')
+async def chatbot(ctx):
+    await discord_bot.user_input_output(ctx, bot)
 
-        # Write the contents of creds to a json file to be read later.
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
-        if os.path.exists('tokens.json'):
-            with open('tokens.json', 'a+') as tokens:
-                with open('token.json', 'r') as token:
-                    credentials = json.load(token)
-                data = json.load(tokens)
-                # Update the credentials in the json data
-                if name in data:
-                    data.update({name: credentials})
-                else:
-                    data[name] = credentials
-                json.dump(data, tokens)
-        # Create json if it does not already exist
-        else:
-            with open('tokens.json', 'w') as tokens:
-                with open('token.json', 'r') as token:
-                    credentials = json.load(token)
-                data = {name: credentials}
-                json.dump(data, tokens)
-"""
 
 bot.run(TOKEN)
