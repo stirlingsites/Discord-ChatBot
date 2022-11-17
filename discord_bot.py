3b656de31c21c53bf6158574d5a2b8881cc3751c
JoeyRacz <54293106+JoeyRacz@users.noreply.github.com>
2022-11-17 14:50:48 -0500

    Deleted unneeded code in discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -17,7 +17,7 @@ def load_json(file):
 responses_data = load_json("bot.json")
 
 
-async def bot_response(user_message, ctx) -> str:
+async def bot_response(user_message) -> str:
     # for each word in user message
     score_list = []
     message_split = re.split(r'\s+|[,;.?!-]\s*', user_message.lower())
@@ -56,55 +56,11 @@ async def bot_response(user_message, ctx) -> str:
     return rand_response.random_string()
 
 
-async def user_input_output(ctx, bot):
-    await ctx.send("Hello, you are talking with a scheduling bot. Please enter a message.")
-    start_message = await bot.wait_for("message", timeout=100)
+async def user_input_output(bot, start_message, mentions_real):
     while start_message:
-        start_message = start_message.content
-        bot_answer = await bot_response(start_message, ctx)
-        await ctx.send(bot_answer)
+        bot_answer = await bot_response(start_message)
+        await mentions_real.send(bot_answer)
         if bot_answer == "See you later!":
             break
-        await ctx.send("Enter another message.")
-        start_message = await bot.wait_for("message", timeout=100)
 
 
-async def scheduler(ctx, bot):
-    await ctx.send("Enter the username of the person you want to schedule an appointment with")
-    username = await bot.wait_for("message", timeout=100)
-    await ctx.send(f'Checking {username.content} calender to see their availability')
-    if not await check_date(ctx, bot):
-        await ctx.send("Date entered is not valid")
-        return
-    await ctx.send("How many minutes would you like your meeting to be?")
-    minutes = await bot.wait_for("message", timeout=100)
-    # TODO check availability of usernames calendar on date for minutes
-    await ctx.send(f"{username.content} is available at these time: option 1 option 2 option 3")
-    await ctx.send("Please select an option (1, 2, or 3)")
-    time_choice = await bot.wait_for("message", timeout=100)
-    await ctx.send("What is the requested meeting going to be about?")
-    subject = await bot.wait_for("message", timeout=100)
-    await ctx.send("Please type any notes you would like to add to the appointment")
-    notes = await bot.wait_for("message", timeout=100)
-    # TODO create the meeting at time_choice with title subject and any notes
-    await ctx.send("Meeting has successfully been created.")
-    return 1
-
-
-async def check_date(ctx, bot):
-    await ctx.send("Please enter the year you would like to meet on (YYYY)")
-    year = await bot.wait_for("message", timeout=100)
-    year = int(year.content)
-    await ctx.send("Please enter the month you would like to meet on (MM)")
-    month = await bot.wait_for("message", timeout=100)
-    month = int(month.content)
-    await ctx.send("Please enter the day you would like to meet on (DD)")
-    day = await bot.wait_for("message", timeout=100)
-    day = int(day.content)
-    valid = False
-    try:
-        new_date = datetime.datetime(year, month, day)
-        valid = True
-    except ValueError:
-        valid = False
-    return valid
